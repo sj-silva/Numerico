9691faec5f2fa75122999ba6e61ce8b7bb3b141e
SJ-Silva <104180091+sj-silva@users.noreply.github.com>
2022-07-18 16:13:17 -0400

    Update count_bit_set.cpp

--- Bitwise/CheckBitSet/count_bit_set.cpp
+++ Bitwise/CheckBitSet/count_bit_set.cpp
@@ -6,12 +6,23 @@ void count_bits(int n) {
         conta = conta + (n & 1);
         n = n >> 1;
     }
-    std::cout << "Total bits: "<<conta << std::endl;
+    std::cout << "Total bits(naive): "<<conta << std::endl;
+}
+
+void brian_kernighan(int n) {
+    int conta = 0;
+    while (n > 0) {
+        n = n & (n - 1);
+        conta++;
+    }
+    std::cout << "Total bits(Brian Kernighan): " << conta << std::endl;
 }
 
 int main()
 {
-    count_bits(16);
+    count_bits(19);
+    brian_kernighan(19);
+
     std::cout << "-- Fim --\n";
     std::cin.get();
     
