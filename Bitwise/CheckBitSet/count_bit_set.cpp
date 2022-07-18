54b572df8e7243d227600768290df5516a51a94e
SJ-Silva <104180091+sj-silva@users.noreply.github.com>
2022-07-18 17:49:14 -0400

    Update count_bit_set.cpp

--- Bitwise/CheckBitSet/count_bit_set.cpp
+++ Bitwise/CheckBitSet/count_bit_set.cpp
@@ -18,10 +18,44 @@ void brian_kernighan(int n) {
     std::cout << "Total bits(Brian Kernighan): " << conta << std::endl;
 }
 
+int table[256];
+
+void initialize() {
+    table[0] = 0;
+    for (int i = 1; i < 256; i++) {
+        // lembre que i/2 = i>>2( caso anterior)
+        //ex: table[5] = 5&1 + table[2] = 1+1 = 2
+        table[i] = (i & 1) + table[i / 2];
+    }
+    
+}
+
+void lookupTable(int n) {
+    
+    int conta = 0;
+    conta+= table[n & 0xff];
+    
+    n = n >> 8;
+    conta += table[n & 0xff];
+   
+    n = n >> 8; 
+    conta += table[n & 0xff];
+    
+    n = n >> 8;
+    conta += table[n & 0xff];
+    
+    std::cout << "Total bits(Lookup Table): " << conta << std::endl;
+}
+
 int main()
 {
-    count_bits(19);
-    brian_kernighan(19);
+    initialize();
+
+    int num = 23;
+    count_bits(num);
+    brian_kernighan(num);
+    lookupTable(num);
+    
 
     std::cout << "-- Fim --\n";
     std::cin.get();
